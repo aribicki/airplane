f76ccd28da925a6a0d50e7aa4ddf503f3200c262
Andrew <aribicki@gmail.com>
2015-02-13 13:29:18 -0500

    Finished adding 4 more planes

---
+++ AirplaneDriver.java
@@ -0,0 +1,32 @@
+public class AirplaneDriver {
+	public static void main(String[] args) {
+	  // Now create an airplane
+	  Airplane myplane = new Airplane("Make","Model",1999,100);
+
+	  Airplane 737 = new Airplane();
+	  plane.name = "737"
+	  plane.make = "Boeing"
+	  plane.model = "737"
+	  plane.year = "1981"
+
+	  Airplane 747 = new Airplane();
+	  plane.name = "747"
+	  plane.make = "Boeing"
+	  plane.model = "747"
+	  plane.year = "1968"
+
+	  Airplane 757 = new Airplane();
+	  plane.name = "757"
+	  plane.make = "Boeing"
+	  plane.model = "757"
+	  plane.year = "1981"
+
+	  Airplane 767 = new Airplane();
+	  plane.name = "767"
+	  plane.make = "Boeing"
+	  plane.model = "767"
+	  plane.year = "1981" 
+
+	  System.out.println(lance.toString());
+}
+	}	
